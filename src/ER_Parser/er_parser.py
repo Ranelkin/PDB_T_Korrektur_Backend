31bb109ab22d3b513326f4269dccba45681971b6
ranelkin <ranelkin23@gmail.com>
2025-04-14 10:50:19 +0200

    Adjusted parser to handle new json submission format

--- src/ER_Parser/er_parser.py
+++ src/ER_Parser/er_parser.py
@@ -11,6 +11,7 @@ logger = setup_logging("er_parser")
 debug_logger = logging.getLogger("er_parser_debug")
 debug_logger.setLevel(logging.DEBUG)
 
+
 def parse_file_ER(path: str, filename: str = None) -> dict:
     """Parses student submission.
 
@@ -21,29 +22,44 @@ def parse_file_ER(path: str, filename: str = None) -> dict:
     Returns:
         dict: parsed student content
     """
-    parsed_file = dict()
+    parsed_graph = dict()
     with open(path, 'r') as file:
-        content = file.read()
-        debug_logger.debug(f" File content: {content} \n\n")
+        content = json.load(file)
+        nodes = content["nodes"]
+        edges = content["edges"]
         
-        # Split in sections, prepare for parsing
-        sections: list[str] = list(map(lambda x: x.lower(), content.split("//"))) # Split in sections 
-        sections: list[list[str]] = [[s.strip() for s in l.split("\n") if s.strip()] for l in sections] # split in lines, strip whitespaces 
-        debug_logger.debug(f"  sections pre parsing: {sections}\n\n")
+        #Map node ID's to the node names 
+        node_ids: list= [node["id"] for node in nodes] #Filter out node ids, scrap the positions 
+        logger.info(f"node id's: {node_ids} \n\n")
         
-        # Process each section
-        for section in sections:
-            # Normalize section text for case-insensitive comparison
-            section_lower: list[str] = list(map(lambda x: x.lower(), section)) # Normalize to lowercase 
-            section_lower: list[str] = list(filter(lambda x: x[0] != '#', section_lower)) # Filter out comment lines 
+        
+        for edge in edges: 
+            #json edge element contents 
+            edge_id: str = edge["id"]
+            source: str = edge["source"] # source ID
+            target: str  = edge["target"] # target ID 
             
-            debug_logger.debug(f" section after making strings lowercase and removing comments: {section_lower}\n\n")
+            #prepare node names 
+            edge_nodes = edge_id.split(" ")[1].split("->")
+            edge_node_source = edge_nodes[0]
+            edge_node_target = edge_nodes[1]
             
-    return parsed_file
+            #create / adjust source entry
+            if parsed_graph.get(edge_node_source): parsed_graph[edge_node_source]["edges"].add(edge_node_target)
+            else: 
+                parsed_graph[edge_node_source] = {"id": source, "edges": set()}
+                parsed_graph[edge_node_source]["edges"].add(edge_node_target)
+                
+            #create / adjust target entry 
+            if not parsed_graph.get(edge_node_target): parsed_graph[edge_node_target] = {"id": target, "edges": set()}
+    
+    return parsed_graph
+       
+        
 
 
 
 if __name__ == '__main__':
-    file_path = "src/er_parser/test_cases/test_case1/test1_correct.txt"
+    file_path = "src/er_parser/test_cases/aggregation.json"
     result = parse_file_ER(file_path)
     print(result)
